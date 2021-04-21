c344b845a1627a9305ae203786e5dff2c448b181
Frank Calabrese <57340460+FrankCalabrese@users.noreply.github.com>
2021-04-21 16:27:41 -0500

    UI canvas. NPC look at player

--- Assets/Scripts/NPCs/States/NPCStateTalking.cs
+++ Assets/Scripts/NPCs/States/NPCStateTalking.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class NPCStateTalking : MonoBehaviour
 {
+    Quaternion startingPos;
+    GameObject player;
+    Vector3 lookHere;
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player");
+        startingPos = transform.rotation;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Mathf.Abs(transform.position.x - player.transform.position.x) < 5 && Mathf.Abs(transform.position.z - player.transform.position.z) < 5)
+        {
+            lookHere = player.transform.position;
+            lookHere.y = transform.position.y;
+            transform.LookAt(lookHere);
+        }
+        else transform.rotation = startingPos;
         
     }
 }
